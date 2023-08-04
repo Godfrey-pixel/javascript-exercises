4d344ca1513bb282807d6225710306151b7bf23c
Godfrey-pixel <thabisothabisogs@gmail.com>
2023-08-04 23:54:10 +0200

    This is exercises, I am learning TDD.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split('');
+    let lettersReversed = letters.reverse();
+    return newString = lettersReversed.join('');
+    
 };
 
 // Do not edit below this line
